73aee30104b2dbcedf32fd1cb179a6d700c39784
Nikhil Yadav <ynikhilprakash@gmail.com>
2025-06-02 03:06:31 +0100

    add projects

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const toggleSwitch = document.getElementById("darkModeToggle");
 toggleSwitch.addEventListener("change", () => {
   document.body.classList.toggle("dark-mode");
 });
+
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  form.reset();
+  alert("Thanks for your message! I'll get back to you soon.");
+});
+
